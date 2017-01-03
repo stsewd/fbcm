2de047c79affc63e716c18f86b9241d041fedb26
Santos Gallegos <santos_g@outlook.com>
2017-01-03 04:12:51 -0500

    Add populate persons with faker

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import random
+
 import click
 from pony import orm
 
@@ -12,6 +14,30 @@ def server():
     from fbcm import app
     orm.sql_debug(app.config['DEBUG'])
     app.run(debug=True)
+
+
+def get_rand_id():
+    return "".join(
+        str(random.randint(0, 9))
+        for _ in range(10)
+    )
+
+
+@cli.command()
+def populate_persons():
+    from faker import Faker
+    from pony.orm import db_session
+    from fbcm.models import Player
+
+    n = 50
+    fake = Faker()
+    with db_session:
+        for i in range(n):
+            Player(
+                id=get_rand_id(),
+                name=fake.first_name(),
+                lastname=fake.last_name()
+            )
 
 
 if __name__ == "__main__":
