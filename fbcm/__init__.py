47f4656c014b16c42fddc7256d599effb4a7a84a
Santos Gallegos <santos_g@outlook.com>
2017-01-07 16:09:00 -0500

    Make jinja render templates without white spaces

--- fbcm/__init__.py
+++ fbcm/__init__.py
@@ -4,4 +4,7 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_object('config.DevelopmentConfig')
 
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
+
 from . import views
